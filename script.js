7d87797250f5448779abbf29f61966402ebd7968
josueramirez1 <josue67ramirez@gmail.com>
2024-04-08 22:34:08 -0700

    Modified script

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Get selectors
-const cartBtn = document.querySelector("button.fixed");
+const cartBtn = document.querySelector(
+  "button.fixed.top-0.right-0.mr-4.mt-4.w-12.bg-blue-500.p-2.rounded-full.text-white"
+);
+const svgCartBtn = document.querySelector("svg");
 const redCounter = document.querySelector("div.rounded-full");
 const closeBtn = document.querySelectorAll("[data-remove-from-cart-button]");
 const color = document.querySelectorAll("h2.text-gray-900.text-lg.font-medium");
@@ -9,3 +12,9 @@ const redItemPrice = document.querySelectorAll(
 const blueItemPrice = document.querySelectorAll(
   "div.flex.items-center.title-font"
 )[1].nextElementSibling;
+
+document.addEventListener("click", (e) => {
+  if (e.target === cartBtn || e.target === svgCartBtn) {
+    console.log("true");
+  }
+});
