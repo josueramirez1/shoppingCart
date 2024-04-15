102853d353d2212299a09c3119e12e863a7bcca1
josueramirez1 <josue67ramirez@gmail.com>
2024-04-15 12:31:12 -0700

    Added addition functionality to shopping cart

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const closeBtn = document.querySelectorAll("[data-remove-from-cart-button]");
 const colorName = document.querySelectorAll(
   "h2.text-gray-900.text-lg.font-medium"
 );
-let priceItem = Array.from(
+let singleItemPrice = Array.from(
   document.querySelectorAll("div.mt-2.flex.justify-between")
 ).map((price) => {
   return price.children[1];
@@ -24,17 +24,19 @@ const colorCart = document.querySelector("div.overflow-y-auto.px-4.pt-4");
 
 const colorBoxes = Array.from(document.querySelectorAll(".mb-6"));
 
-let priceItems = Array.from(
-  document.querySelectorAll("div.flex.items-center.title-font")
-).map((counter) => {
-  return counter.children[1];
-});
+// let priceItems = Array.from(
+//   document.querySelectorAll("div.flex.items-center.title-font")
+// ).map((counter) => {
+//   return counter.children[1];
+// });
 
 // Converting to total price string to number to add or subtract item color prices.
 let totalPriceString = document.querySelectorAll("span.font-bold")[2];
 let totalPriceNum = parseInt(totalPriceString.textContent.substring(1, 3));
 
-console.log(totalPriceNum);
+let singleItemPriceNum = singleItemPrice.map((price) => {
+  return parseInt(price.textContent.substring(1, 3));
+});
 
 // FETCH Json data
 async function getItems(num) {
@@ -53,11 +55,15 @@ async function getItems(num) {
 // cartBtn.classList.add("invisible");
 let count = 2;
 document.addEventListener("click", (e) => {
-  priceItems = Array.from(
-    document.querySelectorAll("div.flex.items-center.title-font")
-  ).map((counter) => {
-    return counter.children[1];
-  });
+  // priceItems = Array.from(
+  //   document.querySelectorAll("div.flex.items-center.title-font")
+  // ).map((counter) => {
+  //   return counter.children[1];
+  // });
+
+  // singleItemPriceNum = singleItemPrice.map((price) => {
+  //   return parseInt(price.textContent.substring(1, 3));
+  // });
 
   // If user clicks on svg button, the shopping cart will appear
   if (e.target === cartBtn || e.target === svgCartBtn) {
@@ -82,6 +88,7 @@ document.addEventListener("click", (e) => {
             colorBox.children[1].children[0].children[0].innerText
           ) {
             // console.log(colorBox.children[1].children[0].children);
+            // If item is already there but you need double of an item, add the counter x2 and multiply price by 2
             if (colorBox.children[1].children[0].children.length === 1) {
               let span = document.createElement("span");
               span.innerText = `x2`;
@@ -91,6 +98,8 @@ document.addEventListener("click", (e) => {
                 "font-bold",
                 "ml-1"
               );
+
+              addSingleAndTotal(color, colorBox);
 
               colorBox.children[1].children[0].append(span);
               count = 2;
@@ -107,7 +116,7 @@ document.addEventListener("click", (e) => {
                     8
                   )
                 );
-
+                addSingleAndTotal(color, colorBox);
                 colorBox.children[1].children[0].children[0].nextElementSibling.innerText = `x${++newCount}`;
                 return;
               }
@@ -140,25 +149,24 @@ function addBlock(color) {
   });
 }
 
-// async function newBlock(color) {
-//   await getItems().then((data) => {
-//     for (let object of data) {
-//       if (color.innerText === object.name) {
-//         let convertToString = object.priceCents.toString();
-//         let truePrice = parseInt(convertToString.substring(0, 2));
-//         let newTruePrice = truePrice + truePrice;
-//         colorCart.innerHTML += createColorBlock(
-//           object.name,
-//           newTruePrice,
-//           object.imageColor
-//         );
-//         totalPriceNum = totalPriceNum + newTruePrice;
-//         totalPriceString.textContent = `$${totalPriceNum}.00`;
-//         // colorCartNames.push(colorCart);
-//       }
-//     }
-//   });
-// }
+function addSingleAndTotal(color, colorBox) {
+  getItems().then((data) => {
+    for (let object of data) {
+      if (color.innerText === object.name) {
+        let convertToString = object.priceCents.toString();
+        let truePrice = parseInt(convertToString.substring(0, 2));
+        let singlePriceItem = parseInt(
+          colorBox.children[1].children[1].innerText.substring(1, 3)
+        );
+        let totalSinglePriceItem = singlePriceItem + truePrice;
+        colorBox.children[1].children[1].textContent = `$${totalSinglePriceItem}.00`;
+        totalPriceNum = totalPriceNum + truePrice;
+        totalPriceString.textContent = `$${totalPriceNum}.00`;
+        // colorCartNames.push(colorCart);
+      }
+    }
+  });
+}
 
 let createColorBlock = (name, price, color) => {
   let result = `<div class="mb-6"><div class="block relative h-24 rounded overflow-hidden"><img alt="ecommerce"class="object-cover object-center w-full h-full block rounded"src="https://dummyimage.com/210x130/${color}/${color}"/><button data-remove-from-cart-button class="absolute top-0 right-0 bg-black rounded-tr text-white w-6 h-6 text-lg flex justify-center items-center">&times;</button></div><div class="mt-2 flex justify-between"><div class="flex items-center title-font"><h2 class="text-gray-900 text-lg font-medium">${name}</h2><span class="text-gray-600 text-sm font-bold ml-1"></span></div><div>$${price}.00</div></div></div>`;
@@ -172,10 +180,5 @@ function tagName(color) {
   <span class="text-gray-600 text-sm font-bold ml-1">x2</span>
 </div>`;
 
-  return result;
-}
-
-function timesNum() {
-  let result = `<span class="text-gray-600 text-sm font-bold ml-1">x2</span>`;
   return result;
 }
