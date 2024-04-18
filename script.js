aaed3949e164541905a08e10389992340655179a
josueramirez1 <josue67ramirez@gmail.com>
2024-04-18 16:56:44 -0700

    Added subtraction to local storage

--- script.js
+++ script.js
@@ -255,6 +255,7 @@ function subtractSingleAndTotal(color, colorBox) {
         } else totalPriceNum = totalPriceNum - truePrice;
         totalPriceString.textContent = `$${totalPriceNum}.00`;
       }
+      toLocalStorage(totalPriceNum);
     }
   });
 }
